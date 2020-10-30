ad334d6504a2b412a4baf1c43cab836b32658a7e
2kai2kai2 <2kai2kai2@gmail.com>
2020-10-30 12:15:30 -0700

    Reorganize and setup test server build configuration
    Reorganized to clean up all the random folders IntelliJ makes for us
    Created build configuration BuildTestServer which will build then run BuildTestServer.bat which runs the spigot buildtools and will setup a server and run it (localhost:25565)
    For copyright and other reasons, these files are in .gitignore
    
    Should we also ignore target?

---
+++ BuildTestServer.bat
@@ -0,0 +1,23 @@
+IF NOT EXIST TestServer\spigot.jar (
+    mkdir BuildTools
+    cd BuildTools
+    curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+    java -jar BuildTools.jar
+
+    cd ..
+    mkdir TestServer
+    copy BuildTools\spigot-1.16.3.jar TestServer\spigot.jar
+
+    cd TestServer
+    echo "eula=true">eula.txt
+    cd ..
+)
+
+cd TestServer
+mkdir plugins
+cd ..
+copy target\sps-mc-link-spigot-1.0-SNAPSHOT-jar-with-dependencies.jar TestServer\plugins\sps-mc-link-spigot-latest.jar
+
+cd TestServer
+java -Xms8g -Xmx8g -XX:+UseG1GC -jar spigot.jar nogui
+pause
--- src/com/github/gcc_minecraft_team/sps_mc_link_spigot/spsspigot.java
+++ src/com/github/gcc_minecraft_team/sps_mc_link_spigot/spsspigot.java
@@ -2,7 +2,7 @@ package com.github.gcc_minecraft_team.sps_mc_link_spigot;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class spsspigot extends JavaPlugin {
+public class SPSSpigot extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new JoinEvent(), this);
