ea2371e9ca1f3356a8d4edb81f3953c28c5eecb8
2kai2kai2 <2kai2kai2@gmail.com>
2020-10-29 23:43:31 -0700

    Setup Spigot and IntelliJ project
    The very basics of a Spigot plugin are setup, using Maven for dependencies (Maven will particularly come in useful later if discord bot integration is added).
    I reccomend using IntelliJ and have setup the project for that.

---
+++ src/main/java/com/github/gcc_minecraft_team/sps_mc_link_spigot/spsspigot.java
@@ -0,0 +1,15 @@
+package com.github.gcc_minecraft_team.sps_mc_link_spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class spsspigot extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
