6a69b98a442dfa5db746e045140a2e1d05ce7320
BuildTools <nlaha@outlook.com>
2020-11-21 15:41:55 -0800

    Fix bat file and switch to Yatopia

--- BuildTestServer.bat
+++ BuildTestServer.bat
@@ -1,11 +1,11 @@
-IF NOT EXIST TestServer\tuinity.jar (
+IF NOT EXIST TestServer\yatopia.jar (
     mkdir BuildTools
     cd BuildTools
-    curl -o tuinity-paperclip.jar https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastSuccessfulBuild/artifact/tuinity-paperclip.jar
+    curl -L -o yatopia-latest.jar https://api.yatopia.net/v2/build/8/download?branch=ver/1.16.4
 
     cd ..
     mkdir TestServer
-    copy BuildTools\tuinity-paperclip.jar TestServer\tuinity.jar
+    copy BuildTools\yatopia-latest.jar TestServer\yatopia.jar
 
     cd TestServer
     echo eula=true>eula.txt
@@ -18,5 +18,5 @@ cd ..
 copy target\sps-mc-link-spigot-1.0-SNAPSHOT-jar-with-dependencies.jar TestServer\plugins\sps-mc-link-spigot-latest.jar
 
 cd TestServer
-java -Xms8g -Xmx24g -XX:ActiveProcessorCount=8 -XX:+UseG1GC -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:9585 -jar yatopia-1.16.4-paperclip-b3.jar nogui
+java -Xms8g -Xmx24g -XX:ActiveProcessorCount=8 -XX:+UseG1GC -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:9585 -jar yatopia.jar nogui
 pause
